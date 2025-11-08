f699753d34618423efe411275eee119c78236393
Liam <125211456+Player23thumb@users.noreply.github.com>
2025-11-08 22:14:48 +0000

    Implement navigation bar in main.py
    Added navigation bar with Home and About items.

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-# imports
 from flask import Flask, render_template, request
+from flask_navigation import Navigation
 import src.calculator_functions as cf
 
 app = Flask(__name__)
+nav = Navigation(app)
 
-# get html file
+nav.Bar('top', [
+    nav.Item('Home', 'index'),
+    nav.Item('About', 'about')
+])
+
 @app.route('/', methods=['GET', 'POST'])
 def index(name=None):
     if request.method == "POST":
             return render_template('index.html')
-        
-
     return render_template('index.html')
+
+@app.route('/about', methods=['GET', 'POST'])
+def index(name=None):
+    if request.method == "POST":
+            return render_template('about.html')
+    return render_template('about.html')
 
 app.run(debug=True)
