218b0c9380e32243a92eec829c3eb1e61cb5233c
Liam <125211456+Player23thumb@users.noreply.github.com>
2025-11-08 19:16:50 +0000

    Initialize Flask app with index route

---
+++ main.py
@@ -0,0 +1,15 @@
+# imports
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+# get html file
+@app.route('/', methods=['GET', 'POST'])
+def index(name=None):
+    if request.method == "POST":
+            return render_template('index.html')
+        
+
+    return render_template('index.html')
+
+app.run(debug=True)
