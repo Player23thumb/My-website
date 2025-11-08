7ca4a1e174dff3af7d2af52d4d625daf19ef972d
Liam <125211456+Player23thumb@users.noreply.github.com>
2025-11-08 21:42:55 +0000

    Add calculator_functions class with initialization

---
+++ src/calculator_functions.py
@@ -0,0 +1,9 @@
+import scipy
+import numpy
+import math
+
+class calculator_functions:
+  def __init__(self, opperation=None, formula=None, **numbers):
+    self.opperation = opperation
+    self.formula = formula
+    self.numbers = numbers
